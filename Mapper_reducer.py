6d435ca1159c67d8f05179de42640a6a85f6ebe9
vaibhav1721 <kaushikvaibhav07@gmail.com>
2018-01-02 11:43:10 +0530

    Reducer updated

--- Mapper_reducer.py
+++ Mapper_reducer.py
@@ -14,6 +14,22 @@ def mapper():
             cleanedData = i.translate(maketrans("","",string.punctuation)).lower()
             print("{0}\t{1}".format(cleanedData, 1))
 
+def reducer():
+    word_count = 0
+    old_key = None
+    for line in sys.stdin:
+        data = line.strip().split("\t")
+        if len(data) != 2:
+            continue
+        this_key,count = data
+        if old_key and old_key != data:
+            print("{0}\t{1}".format(old_key,word_count))
+            word_count = 0
+        old_key = this_key
+        word_count += float(count)
+    if old_key != None:
+        print("{0}\t{1}".format(old_key,word_count))
 
 if __name__ == '__main__':
-    mapper()
+   # mapper()
+    reducer()
